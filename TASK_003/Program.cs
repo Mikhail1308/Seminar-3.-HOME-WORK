7a0a4298d58e6803ebfe62b589fe4798cbf19f90
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-23 11:47:58 +0300

    add TASK_003 Вычисление куба чисел в диапазоне от 1 до N

---
+++ TASK_003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Кубы чисел в диапазоне от 1 до N:  ");
+for (int i = 1; i <= N; i++)
+{
+    int result = i * i * i;
+    System.Console.Write($"{result};  ");
+}
+System.Console.WriteLine();
