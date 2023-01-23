96d207d3aa37139599d811f8f72be5608e11cdc9
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-23 09:22:55 +0300

    add TASK_001 Проверка 5-ти значного числа на палидромность

---
+++ TASK_001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+
+Console.WriteLine("Введите пятизначное число = ");
+int number = int.Parse(Console.ReadLine());
+int pal = 0, num = number;
+while (number > 0) { pal = pal * 10 + number % 10; number /= 10; }
+Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
+System.Console.WriteLine();
