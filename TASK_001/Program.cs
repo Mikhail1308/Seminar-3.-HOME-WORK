4f9d37f3c2b078d99878a5c6fa73fcecaea87ba8
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-24 19:38:07 +0300

    add TASK_001 Проверка числа (палиндром или нет)

--- TASK_001/Program.cs
+++ TASK_001/Program.cs
@@ -1,10 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число 
 // и проверяет, является ли оно палиндромом.
 
-
+int total = 0;
+int t = 0;
+int revers = 0;
 Console.WriteLine("Введите пятизначное число = ");
-int number = int.Parse(Console.ReadLine());
-int pal = 0, num = number;
-while (number > 0) { pal = pal * 10 + number % 10; number /= 10; }
-Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
+int num = int.Parse(Console.ReadLine());
+t = num;
+while (num > 0)
+{
+    revers = num % 10;
+    total = (total * 10) + revers;
+    num = num / 10;
+}
+if (t == total) Console.Write("Данное число - палиндром");
+else Console.WriteLine("Данное число не является палиндромом");
 System.Console.WriteLine();
